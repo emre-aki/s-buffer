32fae85f542ba84212fa7af37c1c54f1c9a03725
Emre Akı <aki.emre@icloud.com>
2025-09-14 18:51:30 +0300

    chore: use our build script in Github workflow
      * Also use a separate dedicated `dist/` subfolder to serve the
        build artifact from

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
+ROOT=$PWD
+DIST_ROOT=$ROOT/dist
+
+rm -rfv $DIST_ROOT
+mkdir $DIST_ROOT
+
+
 if [[ $1 == "-d" ]]; then
-    gcc -shared -DDEBUG ./s_buffer.c -o ./libsbuffer.so -fPIC -v -g
+    gcc -shared -DDEBUG ./s_buffer.c -o $DIST_ROOT/libsbuffer.so -fPIC -v -g
 else
-    gcc -c ./s_buffer.c -o ./s_buffer.o -fPIC -v &&   \
-    gcc -shared ./s_buffer.o -o ./libsbuffer.so -v && \
+    gcc -c ./s_buffer.c -o ./s_buffer.o -fPIC -v &&            \
+    gcc -shared ./s_buffer.o -o $DIST_ROOT/libsbuffer.so -v && \
     rm -rf ./s_buffer.o
 fi
